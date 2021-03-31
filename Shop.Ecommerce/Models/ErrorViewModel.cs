21e317d6f38cf6ca00e860da8d5d2fe48f4e0c3a
QUYEN LY <quyen.ly.151@outlook.com>
2021-03-31 21:29:29 +0700

    Add project files.

---
+++ Shop.Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop.Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
